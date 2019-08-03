1b3a022edb87318664a6772a3d5dc491010652f5
unknown <HONADMIN@IE11LT5W74N62.global.ds.honeywell.com>
2019-08-03 15:22:43 +0530

    login-screen

--- smart-home/src/app/app.module.ts
+++ smart-home/src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+
+const appRoute: Routes = [{
+  path: 'login',
+  component: 'LoginComponent'
+},
+{
+  path: 'dashboard',
+  component: 'DashboardComponent'
+},
+{
+  path: '**',
+  pathMatch: 'full',
+  redirectTo: 'login'
+}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoute
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
